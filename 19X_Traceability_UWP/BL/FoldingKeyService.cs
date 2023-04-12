a8a81387f7dba1822aa87221cd48710d121ee425
martinbednar <martin.bedn20@gmail.com>
2023-04-12 12:49:05 +0200

    Add FK service.

---
+++ 19X_Traceability_UWP/BL/FoldingKeyService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using _19X_Traceability_UWP.DAL;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using Windows.Storage;
+
+namespace _19X_Traceability_UWP.BL
+{
+    internal class FoldingKeyService
+    {
+        public async void ExportFoldingKeys(DateTime from, DateTime to)
+        {
+            string fromString = from.ToString("dd.MM.yyyy");
+            string toString = to.ToString("dd.MM.yyyy");
+
+            string query = @"SELECT
+                FK.id,
+                dateTime,
+                SN,
+                SKP,
+                custNr,
+                keyCode,
+                result AS resultId,
+                text AS resultDescription,
+                pcInSet,
+                actCnt,
+                home,
+                ins,
+                keyPress,
+                spirollPress,
+                takeOut,
+                forceKey,
+                forceSpiroll,
+                notEnough,
+                tooMuch,
+                spirolDepth
+                FROM FK LEFT OUTER JOIN alarmFK ON alarmFK.id = FK.result
+                ORDER BY FK.id ASC;";
+
+            DbHandler dbHandler = new DbHandler();
+            SqlCommand sqlCmd = dbHandler.CreateSqlCmd(query);
+
+            sqlCmd.Parameters.Clear();
+            sqlCmd.Parameters.AddWithValue("@from", fromString);
+            sqlCmd.Parameters.AddWithValue("@to", toString);
+
+            using (SqlDataReader reader = sqlCmd.ExecuteReader())
+            {
+                //string fileName = "I:\\test.csv";
+                //StreamWriter sw = new StreamWriter(fileName);
+                //object[] output = new object[reader.FieldCount];
+
+                StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(@"I:\");
+                StorageFile file = await folder.CreateFileAsync("test.csv",
+                    CreationCollisionOption.ReplaceExisting);
+                
+                string outputStr = String.Empty;
+
+                object[] output = new object[reader.FieldCount];
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                    output[i] = reader.GetName(i);
+
+                outputStr += string.Join(",", output) + "\n";
+
+                while (reader.Read())
+                {
+                    reader.GetValues(output);
+                    outputStr += string.Join(",", output) + "\n";
+                }
+
+
+
+                await FileIO.WriteTextAsync(file, outputStr);
+                
+                reader.Close();
+            }
+
+            dbHandler.CloseDbConn();
+        }
+    }
+}
