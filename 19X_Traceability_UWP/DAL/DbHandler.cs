9ef46bc46e88dc506fc643b2307fff636a21a599
martinbednar <martin.bedn20@gmail.com>
2023-04-12 12:49:21 +0200

    Add DB Handler.

---
+++ 19X_Traceability_UWP/DAL/DbHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+namespace _19X_Traceability_UWP.DAL
+{
+    internal class DbHandler
+    {
+        private readonly string _connectionString = "Server=K2007;Database=19X;user id=dacia;password=PrestrojenaDacia19X";
+
+        private SqlConnection _dbConnection;
+
+        public SqlCommand CreateSqlCmd(string query)
+        {
+            SqlCommand sqlCmd = new SqlCommand(query);
+
+            _dbConnection = new SqlConnection(_connectionString);
+            _dbConnection.Open();
+            sqlCmd.Connection = _dbConnection;
+
+            return sqlCmd;
+        }
+
+        public void CloseDbConn()
+        {
+            _dbConnection.Close();
+        }
+    }
+}
